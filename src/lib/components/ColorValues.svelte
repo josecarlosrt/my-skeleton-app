52460adf8c5b4da150e050e5ef475e27b4a2ad6c
josecarlosrt <59973113+josecarlosrt@users.noreply.github.com>
2024-01-12 00:37:26 -0600

    created color values component

---
+++ src/lib/components/ColorValues.svelte
@@ -0,0 +1,19 @@
+<script>
+
+</script>
+
+<div>
+    <h3 class="h3">Get Started</h3>
+	<p>How may colors would you like to combine?</p> 
+</div>
+<label class="label">
+	<span>Select</span>
+	<select class="select">
+		<option value="1">1</option>
+		<option value="2">2</option>
+		<option value="3">3</option>
+		<option value="4">4</option>
+		<option value="5">5</option>
+        <option value="6">6</option>
+	</select>
+</label>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,11 +1,13 @@
 <!-- YOU CAN DELETE EVERYTHING IN THIS PAGE -->
 <script lang="ts">
+	import ColorValues from "$lib/components/ColorValues.svelte";
+
 
 </script>
 
 
 <div class="container h-full mx-auto flex justify-center items-center">
-	<div class="space-y-5">	
-
+	<div class="card p-4 flex flex-col gap-3">
+		<ColorValues />
 	</div>
 </div>
