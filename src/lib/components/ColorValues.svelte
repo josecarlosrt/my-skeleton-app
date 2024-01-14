ff4318a3e13837c20fdbe22de9cb0acac7311d3c
josecarlosrt <59973113+josecarlosrt@users.noreply.github.com>
2024-01-13 23:48:34 -0600

    added color input grid - minor fixes

--- src/lib/components/ColorValues.svelte
+++ src/lib/components/ColorValues.svelte
@@ -5,15 +5,21 @@
 
 <div class="container">
 	<h3 class="h3 text-center pb-4">Get Started!</h3>
-<label class="label inline-flex">
-	<span>How may colors would you like to combine?</span>
-	<select class="select w-1/6">
-		<option value="1">1</option>
-		<option value="2">2</option>
-		<option value="3">3</option>
-		<option value="4">4</option>
-		<option value="5">5</option>
-		<option value="6">6</option>
-	</select>
-</label>
+	<label class="label text-center">
+		<span>How may colors would you like to combine?</span>
+		<select class="select text-center w-16">
+			<option value="1">1</option>
+			<option value="2">2</option>
+			<option value="3">3</option>
+			<option value="4">4</option>
+			<option value="5">5</option>
+			<option value="6">6</option>
+		</select>
+	</label>
+</div>
+
+<div class="grid grid-cols-2 md:grid-cols-3 gap-4">
+    <div class="flex">
+        <input class="input" type="text" />
+    </div>
 </div>
